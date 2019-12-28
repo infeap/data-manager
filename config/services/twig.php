4586e2857ccaefe72acc0023037c5c0bedc69696
Tobias Krebs <t.krebs@ep-3.de>
2019-12-28 14:48:02 +0100

    Add basic Twig templating configuration

---
+++ config/services/twig.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of the
+ * Infeap Data Manager (https://www.infeap.org/data-manager)
+ * open source project.
+ *
+ * @copyright   2018-2020 Tobias Krebs and the Infeap Team
+ * @license     https://www.gnu.org/licenses/gpl.html GNU General Public License 3
+ */
+
+/*
+ * See init/app-array.php for available $app keys
+ */
+return function (array $app): array {
+
+    return [
+        'services' => [
+            'app_config' => [
+                'templates' => [
+                    'extension' => 'twig',
+                    'paths' => [
+                        'page' => $app['dir'] . '/templates/page/',
+                    ],
+                ],
+                'twig' => [
+                    'cache_dir' => $app['checks']['cache_dir_is_writable'] ? $app['config']['cache_dir'] . '/templates/' : false,
+                    'extensions' => [
+                        // \Infeap\Foundation\Template\Twig\AssetExtension::class,
+                    ],
+                    'globals' => [
+                        'app' => $app,
+                    ],
+                    'strict_variables' => false,
+                ],
+            ],
+        ],
+    ];
+};
