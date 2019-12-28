714180a636c479e9e1dc86403bccb2d3661e8d74
Tobias Krebs <t.krebs@ep-3.de>
2019-12-28 15:28:12 +0100

    Add automatic cache deletion after Git commits, checkouts and merges

---
+++ bin/delete-cache.js
@@ -0,0 +1,40 @@
+/**
+ * This file is part of the
+ * Infeap Data Manager (https://www.infeap.org/data-manager)
+ * open source project.
+ *
+ * @copyright   2018-2020 Tobias Krebs and the Infeap Team
+ * @license     https://www.gnu.org/licenses/gpl.html GNU General Public License 3
+ */
+
+'use strict'
+
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+
+let appDir = path.dirname(__dirname)
+
+let appPackageFile = path.join(appDir, '/package.json')
+
+try {
+    fs.accessSync(appPackageFile, fs.R_OK)
+} catch (error) {
+    console.error(appPackageFile + ' does not exist or is not readable')
+    process.exit(1)
+}
+
+let appPackage = require(appPackageFile)
+
+if (! appPackage.name || appPackage.name != 'infeap/data-manager') {
+    console.error(appPackageFile + ' does not belong to the Infeap Data Manager project')
+    process.exit(2)
+}
+
+let globPattern = path.join(appDir, '/var/cache/version-*')
+
+for (let versionedCacheDir of glob.sync(globPattern)) {
+    fs.rmdirSync(versionedCacheDir, {
+        recursive: true,
+    })
+}
