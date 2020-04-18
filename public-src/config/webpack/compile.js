eed93a6b395ad18469f9b6a833ffdd8aadbe9b22
Tobias Krebs <t.krebs@ep-3.de>
2020-04-18 06:25:00 +0200

    Setup Webpack based asset compilation

---
+++ public-src/config/webpack/compile.js
@@ -0,0 +1,137 @@
+'use strict'
+
+const glob = require('glob')
+const path = require('path')
+
+const Autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = function (env) {
+
+    if (env.production) {
+        env.vars = {
+            fileExtensionPrefix: '.min',
+            sassOptions: {},
+            mode: 'production',
+        }
+    }
+
+    if (env.debug) {
+        env.vars = {
+            fileExtensionPrefix: '',
+            sassOptions: {
+                outputStyle: 'expanded',
+                sourceComments: true,
+                sourceMapEmbed: true,
+            },
+            mode: 'development',
+        }
+    }
+
+    return {
+        entry() {
+            let files = {}
+
+            for (let file of glob.sync('public-src/js/*.js')) {
+                files[path.basename(file, '.js')] = './' + file
+            }
+
+            return files
+        },
+        output: {
+            filename: 'js/compiled/[name]' + env.vars.fileExtensionPrefix + '.js',
+            chunkFilename: 'js/compiled/[name]' + env.vars.fileExtensionPrefix + '.js?v=[hash]',
+            path: path.resolve('public'),
+        },
+        module: {
+            rules: [
+                {
+                    include: [
+                        path.resolve('public-src/js/'),
+                    ],
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                            presets: [
+                                ['@babel/preset-env', {
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                }],
+                            ],
+                        },
+                    },
+                },
+                {
+                    include: [
+                        path.resolve('public-src/js/'),
+                    ],
+                    test: /\.vue$/,
+                    use: [
+                        'vue-loader',
+                    ],
+                },
+                {
+                    include: [
+                        path.resolve('public-src/css/'),
+                        path.resolve('public-src/js/'),
+                    ],
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    require('autoprefixer'),
+                                ],
+                            },
+                        },
+                        {
+                            loader: 'string-replace-loader',
+                            options: {
+                                search: 'url\\(([a-z\\-]+)\\/',
+                                replace: 'url(../../$1/',
+                                flags: 'gi',
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: env.vars.sassOptions,
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            Autoprefixer,
+            new MiniCssExtractPlugin({
+                filename: 'css/compiled/[name]' + env.vars.fileExtensionPrefix + '.css',
+                chunkFilename: 'css/compiled/[name]' + env.vars.fileExtensionPrefix + '.css?v=[hash]',
+            }),
+            new VueLoaderPlugin(),
+        ],
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js',
+            },
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+        },
+        devtool: false,
+        mode: env.vars.mode,
+    }
+}
