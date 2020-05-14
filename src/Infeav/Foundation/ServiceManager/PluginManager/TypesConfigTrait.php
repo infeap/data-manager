1ae2257349db1cd2fb79bc32a833864b679b3f8f
Tobias Krebs <t.krebs@ep-3.de>
2020-05-14 22:18:12 +0200

    Add internal standard for configuration of typed plugin managers

---
+++ src/Infeav/Foundation/ServiceManager/PluginManager/TypesConfigTrait.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the
+ * Infeav Data Manager (https://www.infeav.org/data-manager)
+ * open source project
+ *
+ * @copyright   2018-2020 Tobias Krebs and the Infeav Team
+ * @license     https://www.gnu.org/licenses/gpl.html GNU General Public License 3
+ */
+
+namespace Infeav\Foundation\ServiceManager\PluginManager;
+
+use Laminas\ServiceManager\ServiceManager;
+
+trait TypesConfigTrait
+{
+
+    public function getTypesServiceConfig(ServiceManager $serviceManager, string $pluginManagerName): array
+    {
+        $typesServiceConfig = [
+            'invokables' => [],
+            'factories' => [],
+        ];
+
+        $typesConfig = $serviceManager->get('config')[$pluginManagerName]['types'] ?? null;
+
+        $endsWith = function (string $haystack, string $needle): bool {
+            $length = strlen($needle);
+
+            if ($length == 0) {
+                return true;
+            }
+
+            return (substr($haystack, -$length) === $needle);
+        };
+
+        foreach ($typesConfig as $type => $typeServiceName) {
+            if ($endsWith($typeServiceName, 'Factory')) {
+                $typesServiceConfig['factories'][] = $typeServiceName;
+            } else {
+                $typesServiceConfig['invokables'][] = $typeServiceName;
+            }
+        }
+
+        return $typesServiceConfig;
+    }
+
+}
