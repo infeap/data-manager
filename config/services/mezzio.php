d5b072b48a4878f9f366fed1ef37ff2e0541a357
Tobias Krebs <t.krebs@ep-3.de>
2020-05-24 21:42:21 +0200

    Fix Mezzio debug config key

--- config/services/mezzio.php
+++ config/services/mezzio.php
@@ -8,21 +8,28 @@
  * @license     https://www.gnu.org/licenses/gpl.html GNU General Public License 3
  */
 
-return [
-    \Mezzio\ConfigProvider::class,
-    \Mezzio\Helper\ConfigProvider::class,
-    \Mezzio\Router\ConfigProvider::class,
-    \Mezzio\Router\FastRouteRouter\ConfigProvider::class,
-    \Mezzio\Twig\ConfigProvider::class,
+/*
+ * See init/app-array.php for available $app keys
+ */
+return function (array $app): array {
 
-    'services' => [
-        'config' => [
-            'mezzio' => [
-                'error_handler' => [
-                    'template_404' => 'page::error/404',
-                    'template_error' => 'page::error/500',
+    return [
+        \Mezzio\ConfigProvider::class,
+        \Mezzio\Helper\ConfigProvider::class,
+        \Mezzio\Router\ConfigProvider::class,
+        \Mezzio\Router\FastRouteRouter\ConfigProvider::class,
+        \Mezzio\Twig\ConfigProvider::class,
+
+        'services' => [
+            'config' => [
+                'debug' => $app['config']['debug'],
+                'mezzio' => [
+                    'error_handler' => [
+                        'template_404' => 'page::error/404',
+                        'template_error' => 'page::error/500',
+                    ],
                 ],
             ],
         ],
-    ],
-];
+    ];
+};
